ba9409bced1c2b7e4778e154e258f090c47136dd
root <root@instance-1.northamerica-northeast1-a.c.proven-hash-284203.internal>
2021-03-16 20:21:19 +0000

    updated new code

--- src/main/java/com/rest/microserviceb/GreetingController.java
+++ src/main/java/com/rest/microserviceb/GreetingController.java
@@ -1,7 +1,14 @@
 package com.rest.microserviceb;
 
+import java.io.IOException;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.methods.RequestBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,15 +17,41 @@ import org.springframework.web.bind.annotation.RestController;
 public class GreetingController {
 
 	//private static final String template = "Hello, %s!";
-	private final AtomicLong counter = new AtomicLong();
+	//private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/callMicroserviceB")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "Response From MicroserviceB") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(name));
+	public String greeting(@RequestParam(value = "message", defaultValue = "Default Response") String message) {
+		return message;
 	}
 	
-	@GetMapping("/AcallsMicroserviceB")
-	public Greeting greetingAB(@RequestParam(value = "name", defaultValue = "Request Received from MicroserviceA and Response Sent From MicroserviceB") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(name));
+	@GetMapping("/BcallMicroserviceA")
+	public String greetingBA(@RequestParam(value = "message", defaultValue = "Default Response") String message) {
+		
+		try {
+			CloseableHttpClient httpclient = HttpClients.createDefault();
+			RequestBuilder reqbuilder = RequestBuilder.get();
+			
+			RequestBuilder reqbuilder1 = reqbuilder.setUri("http://microservice-a:8080/callMicroserviceA");
+			RequestBuilder reqbuilder2 = reqbuilder1
+		    		.addParameter("message",message);
+			
+			HttpUriRequest httpGet = reqbuilder2.build();
+			HttpResponse httpresponse;
+			httpresponse = httpclient.execute(httpGet);
+			
+			
+			 String responseBody = EntityUtils.toString(httpresponse.getEntity());
+			 
+			 if(httpresponse.getStatusLine().getStatusCode() == 200) {
+				 return responseBody;
+			 }else {
+				 return "Error Contacting Microservice A from Microservice B";
+			 }
+		 
+		} catch (IOException e) {
+			e.printStackTrace();
+			return "Error Contacting Microservice A from Microservice B";
+		}
+	
 	}
 }
